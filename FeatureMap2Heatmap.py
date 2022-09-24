05319bbaab9441c70133173b7007b13af4700c2d
yumiaomiao-a <80734248+yumiaomiao-a@users.noreply.github.com>
2022-09-24 21:01:34 +0800

    Update FeatureMap2Heatmap.py

--- FeatureMap2Heatmap.py
+++ FeatureMap2Heatmap.py
@@ -1,9 +1,6 @@
 import matplotlib.pyplot as plt
 import torch
 
-# FeatureMap2Heatmap(images, x_res,x_org_f,x_Block1, x_Block2, x_Block3, x_Block4, x_construct_240,x_Block11, x_Block22, x_Block33, x_Block44)
-# feature  -->   [ batch, channel, height, width ]
-
 
 def featuremap2heatmap(x, x_res,x_org_f,feature1, feature2, feature3,feature4,feature11, feature22, feature33,feature44):
     ## initial images
@@ -16,7 +13,6 @@ def featuremap2heatmap(x, x_res,x_org_f,feature1, feature2, feature3,feature4,fe
     ax = fig.add_subplot(111)
     plt.imshow(heatmap)
     plt.colorbar()
-    # plt.savefig(args.log+'/'+args.log + '_x_visual.jpg')
     plt.savefig('./visualmap/input_visual.jpg')
     plt.close()
 
@@ -30,7 +26,6 @@ def featuremap2heatmap(x, x_res,x_org_f,feature1, feature2, feature3,feature4,fe
     ax = fig.add_subplot(111)
     plt.imshow(heatmap)
     plt.colorbar()
-    # plt.savefig(args.log+'/'+args.log + '_x_visual.jpg')
     plt.savefig('./visualmap/x_res_visual.jpg')
     plt.close()
 
@@ -44,10 +39,8 @@ def featuremap2heatmap(x, x_res,x_org_f,feature1, feature2, feature3,feature4,fe
     ax = fig.add_subplot(111)
     plt.imshow(heatmap)
     plt.colorbar()
-    # plt.savefig(args.log+'/'+args.log + '_x_visual.jpg')
     plt.savefig('./visualmap/x_org_f_visual.jpg')
     plt.close()
-
 
 
     ## first feature
@@ -75,6 +68,7 @@ def featuremap2heatmap(x, x_res,x_org_f,feature1, feature2, feature3,feature4,fe
     plt.savefig('./visualmap/y_Block1_visual.jpg')
     plt.close()
 
+    
     ## second feature
     feature_first_frame = feature2[0,:,:,:].cpu()    ## the middle frame
     heatmap = torch.zeros(feature_first_frame.size(1), feature_first_frame.size(2))
@@ -101,6 +95,7 @@ def featuremap2heatmap(x, x_res,x_org_f,feature1, feature2, feature3,feature4,fe
     plt.close()
 
 
+    
     ## third feature
     feature_first_frame = feature3[0,:,:,:].cpu()    ## the middle frame
     heatmap = torch.zeros(feature_first_frame.size(1), feature_first_frame.size(2))
@@ -115,8 +110,6 @@ def featuremap2heatmap(x, x_res,x_org_f,feature1, feature2, feature3,feature4,fe
     plt.colorbar()
     plt.savefig('./visualmap/x_Block3_visual.jpg')
     plt.close()
-
-
 
     feature_first_frame = feature33[0,:,:,:].cpu()    ## the middle frame
     heatmap = torch.zeros(feature_first_frame.size(1), feature_first_frame.size(2))
@@ -157,17 +150,3 @@ def featuremap2heatmap(x, x_res,x_org_f,feature1, feature2, feature3,feature4,fe
     plt.savefig('./visualmap/y_Block4_visual.jpg')
     plt.close()
 
-
-
-
-    ## third feature
-    # heatmap2 = torch.pow(map_x[0,:,:],2)    ## the middle frame
-    #
-    # heatmap2 = heatmap2.data.cpu().numpy()
-    #
-    # fig = plt.figure()
-    # ax = fig.add_subplot(111)
-    # plt.imshow(heatmap2)
-    # plt.colorbar()
-    # plt.savefig('./_x_DepthMap_visual.jpg')
-    # plt.close()
