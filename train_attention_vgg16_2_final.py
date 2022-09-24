1135e086899bee56a1cbc9af2cf991f98123226a
yumiaomiao-a <80734248+yumiaomiao-a@users.noreply.github.com>
2022-09-24 16:38:16 +0800

    Update train_attention_vgg16_2_final.py

--- train_attention_vgg16_2_final.py
+++ train_attention_vgg16_2_final.py
@@ -7,41 +7,35 @@ import torchvision
 from torch.autograd import Variable
 from torchvision import datasets, transforms
 import numpy as np
-#import transforms
-#from transforms import
-import os            # os包集成了一些对文件路径和目录进行操作的类
+import os           
 import matplotlib.pyplot as plt
 import time
-# from model import Model1,Model2,Model3
 import torch.nn as nn
 os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
-# os.environ['CUDA_VISIBLE_DEVICES']='0'
 from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
-# from data_augmentation import face_eraser_gray
 import numpy as np
 from sklearn.metrics import roc_auc_score
-# from highfilter import idea_high_filter
 from regionlabel import region_label,fakelabel_new,reallabel_new,fakelabel_new_30,reallabel_new_30,region_label
 import matplotlib.pyplot as plt
-# os.environ['CUDA_LAUNCH_BLOCKING']='1'
 from regionlabel import reallabel_new_120,reallabel_new_60,fakelabel_new_120,fakelabel_new_60,reallabel_new_15,fakelabel_new_15
 from sklearn import metrics
 from torch.optim.lr_scheduler import CosineAnnealingLR
 from data_augmentation import face_eraser_gray,bg_eraser_gray,face_eraser_shuffle,bg_eraser_shuffle,face_eraser_change,bg_eraser_change
-
+from FeatureMap2Heatmap import featuremap2heatmap
+from sklearn.metrics.pairwise import cosine_similarity
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 
 
-# 读取数据
+# read data
 data_dir = './ffdfc23'
 # data_dir = '../celeb-df-120-60(1.3)'
 # data_dir = '../timit-lq-10000-2800'
 # data_dir = '../data-400-train-test'
-# data_dir = '../second paper/face_dect/new_add_exp_FF++/F2F/c23'
+# data_dir = '../new_add_exp_FF++/F2F/c23'
 # data_dir = '../new_add_exp_FF++/DF/c23'
 # data_dir = '../ff_all_new/data_c23'
 # data_dir = '../new_add_exp_FF++_DF_c23'
@@ -58,8 +52,6 @@ data_transform = {
         # transforms.Normalize([0.485,0.456,0.406],[0.229,0.224,0.225]),
         # transforms.RandomErasing(p=0.5,scale=(0.02,0.33),ratio=(0.3,0.3),value=0,inplace=False)
     ]),
-
-
     'test':transforms.Compose([
         transforms.Scale([240,240]),
         transforms.ToTensor(),
@@ -69,44 +61,35 @@ data_transform = {
 
 
 image_datasets = {x:datasets.ImageFolder(root = os.path.join(data_dir,x),
-                                         transform = data_transform[x]) for x in ['train', 'test']}
+                                         transform = data_transform[x]) for x in ['train', 'val']}
 
 train_set = image_datasets['train']
-test_set = image_datasets['test']
+val_set = image_datasets['val']
 
-# 这一步相当于读取数据
-batch_size =6
+batch_size =16
 dataloader = {x:torch.utils.data.DataLoader(dataset = image_datasets[x],
-                                            batch_size = 6,
-                                            shuffle = True,drop_last=True) for x in ['train','test'] }  # 读取完数据后，对数据进行装载
+                                            batch_size = 16,
+                                            shuffle = True,drop_last=True) for x in ['train','val'] } 
 
 
 train_dataloader = dataloader['train']
-test_dataloader = dataloader ['test']
+val_dataloader = dataloader ['val']
 
-dataset_size = {x:len(image_datasets[x]) for x in ['train','test']}
-
-
-#baseline_1
+dataset_size = {x:len(image_datasets[x]) for x in ['train','val']}
 
 
-# from model_new_attention import resautoencoder,resautoencoder_new,resautoencoder_new_vgg16
-# model = resautoencoder_new_vgg16
-
-from CDCN_model import CDCNpp,CDCN_my
+from CDCN_model import CDCN_my
 model = CDCN_my
 
-############################################加载训练好的模型，多卡训练时参数前面会加上module,要手动去掉
-# dic = torch.load('./save_final_cdcn/df_0.1.pth')  #the best para   ff23_8.pth
-dic = torch.load('./save_final_cdcn/df_0.1.pth')  #the best para
-from collections import OrderedDict
-new_state_dict = OrderedDict()
-for k,v in dic.items():
-    name = k[7:]
-    new_state_dict[name] = v
-model.load_state_dict(new_state_dict)
 
-
+###加载训练好的模型，多卡训练时参数前面会加上module,要手动去掉
+# dic = torch.load('./save_model/df_0.1.pth')  #the best para
+# from collections import OrderedDict
+# new_state_dict = OrderedDict()
+# for k,v in dic.items():
+#     name = k[7:]
+#     new_state_dict[name] = v
+# model.load_state_dict(new_state_dict)
 
 
 def adjust_learning_rate(epoch):
@@ -117,54 +100,27 @@ def adjust_learning_rate(epoch):
         lr = lr / 100
     elif epoch > 15:
         lr = lr / 1000
-    # elif epoch > 12:
-    #     lr = lr / 10000
-    # elif epoch > 15:
-    #     lr = lr / 100000
 
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
-from SSIM_my import SSIM
-from loss import WeightedFocalLoss
 loss_f = torch.nn.CrossEntropyLoss()
-#WeightedFocalLoss()
-#torch.nn.CrossEntropyLoss()
-
-
-
 l1loss = nn.L1Loss()
 mseloss = nn.MSELoss()
 smoothl1 = nn.SmoothL1Loss()
 
-
-
-#loss_f = torch.nn.CosineEmbeddingLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.00002)
-# scheduler = CosineAnnealingLR(optimizer,T_max=2)
-# optimizer2 = torch.optim.Adam(model2.parameters(), lr=0.00001)
-# optimizer3 = torch.optim.Adam(model3.parameters(), lr=0.00001)
-# optimizer = torch.optim.SGD(model.parameters(), lr=0.0002)
-# Use_gpu = torch.cuda.is_available()
-# if Use_gpu:
-#     model = model.cuda()
 
-
-model = torch.nn.DataParallel(model,device_ids=[0,1])
+model = torch.nn.DataParallel(model,device_ids=[0,1]) #trained with two GPUs
 model = model.cuda()
-# device1 = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
-#device2 = torch.device('cuda:2' if torch.cuda.is_available() else 'cpu')
-# device3 = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-# model.to(device3)
-
 
 epoch_n = 20
 
 
+# save root of the trained model
 def save_models(epoch):
-    torch.save(model.state_dict(), "./save_final_cdcn/lossweight/df_10p0.1g_{}.pth".format(epoch))
+    torch.save(model.state_dict(), "./save_model/model_{}.pth".format(epoch))
 print("Chekcpoint saved")
-
 
 
 class ConsistencyCos(nn.Module):
@@ -182,152 +138,61 @@ class ConsistencyCos(nn.Module):
         loss = self.mse_fn(cos, labels)
         return loss
 
-
 ConsistencyCos = ConsistencyCos()
 
-import cv2 as cv
-from torchvision.utils import save_image
-def test():
+
+
+#######################evaluation
+def val():
     model.eval()
-    # model2.eval()
-    # model3.eval()
-    test_acc = 0.0
+    val_acc = 0.0
     prob_all = []
     label_all = []
     prob_all_soft = []
-    for i, (images, labels) in enumerate(test_dataloader):
-        # images = images.to(device3)
-        # labels = labels.to(device3)
+    for i, (images, labels) in enumerate(val_dataloader):
+        
         images = images.cuda()
         labels1 = labels.cuda()
         labels = labels.numpy().astype(np.float)
 
-        #########################生成频域图和二进制标签
-        # images_f = []
-        # binary_label=[]
-        # for j in range(len(images)):
-        #     # print("(((((((9)))))))",images[j])
-        #
-        #     f = idea_high_filter(images[j])
-        #     images_f.append(f)
-        #
-        # images_f = torch.tensor(images_f)
-        # images_f = images_f.cuda()
-
-
-        '''
-        a = labels.shape
-        # print(a)
-        if a!=32:
-            labels = torch.ones(32)
-        else:
-            labels = labels
-        labels = torch.tensor(labels, dtype=torch.long)
-        labels = labels.to(device3)
-        '''
-
         with torch.no_grad():
-            # x_res_4,x_f_4,w_res_c,w_f_c,x_construct_240,x_res,x_Block1,x_Block2,x_Block3,x_Block4,x_org_f,x_Block11,x_Block22,x_Block33,x_Block44 = model(images)
-
-            # w_f_c = model(images)
-            # x_,w_res_c,w_f_c,x_res_4,x_f_4,x_construct_30,x_construct_120,x_construct_60,x_construct_15,x_res,x_Block1,x_Block2,x_Block3,x_Block4,x_org_f,x_Block11,x_Block22,x_Block33,x_Block44= model(images)
             x_consis,x_,w_res_c,w_f_c,w_res_c4,w_f_c4,x_res_4,x_f_4,x_construct_30,x_construct_120,x_construct_60,x_construct_15,y_construct_30,y_construct_120,y_construct_60,y_construct_15,x_res,x_Block1,x_Block2,x_Block3,x_Block4,x_org_f,x_Block11,x_Block22,x_Block33,x_Block44= model(images)
 
         pred = w_f_c4 + w_res_c4
 
         _, prediction = torch.max(pred.data, 1)
 
-        test_acc += torch.sum(prediction == labels1.data)
+        val_acc += torch.sum(prediction == labels1.data)
 
-
-        # w_res_c = concat_c
         pred = torch.sigmoid(pred).cpu()
-        # print('＋＋＋＋＋＋＋＋＋＋＋',pred.shape,type(pred))
 
         w_res_c = torch.max(pred,1)[0].cpu().numpy()
         w_res_cc = torch.max(pred,1)[1].cpu().numpy()
-        # print('//=========',w_res_c.shape,w_res_c)
-        # print('777777=========',w_res_cc.shape,w_res_cc)
-        #
-        # print('------=========',pred.shape,pred)
-
-
-        # w_res_c_1 = w_res_c[:,0]
-        # w_res_c_1 = np.amax(w_res_c,axis=1)
-
-
-        # w_res_c_2 = w_res_c.view(1,-1).squeeze(0)
-        # print('=========',w_res_c_1)
-
-        # w_res_c_2 = w_res_c_2.data.cpu().numpy()
-
-        # print('-----------',w_res_c_1)
-
-        # out_pred = np.zeros((w_res_c.shape[0]),dtype=np.float)
-
-        # for i in range(w_res_c.shape[0]):
-        #     out_pred[i]=torch.max(pred,1)[1].numpy()
+       
         out_pred = w_res_cc
-        # if w_res_c_1[i]>0.5:
-        #     out_pred[i]=1.0
-        # else:
-        #     out_pred[i]=0.0
-
-        # tol_pred = np.concatente((tol_pred,out_pred))
-
-
-        # _, prediction = torch.max(w_res_c.data, 1)
-
-
-        # prediction = prediction1 + prediction2 + prediction3
-        # prediction = torch.where(prediction >= 2, 1, 0)
-        # test_acc += torch.sum(prediction == labels.data)
-
+        
         prob_all.extend(out_pred)
         label_all.extend(labels)
         prob_all_soft.extend(pred)
 
 
-        # prediction = prediction.numpy()
-        # labels = labels.numpy()
-        # print('77777__________', type(prob_all_soft))
-        # print('__________', type(labels))
-        # auc_score1 = roc_auc_score(labels, prediction)
-
-    # Compute the average acc and loss over all 10000 test images
-    # test_acc = test_acc / len(test_set)
-
     label_all = np.array(label_all).astype(np.int64)
     prob_all = np.array(prob_all).astype(np.int64)
-    # prob_all_soft = np.array(prob_all_soft).astype(np.float32)
-
-    # print('4477777__________', type(prob_all_soft))
-    # print('============',label_all.dtype,prob_all.dtype,prob_all_soft.dtype)
+    
     acc_valid = metrics.accuracy_score(label_all, prob_all)
     label_all = label_all.astype(np.float32)
 
-    # prob_all_soft = prob_all_soft.cpu()
     prob_all_soft = torch.tensor([item.cpu().detach().numpy() for item in prob_all_soft])
 
     auc_valid = metrics.roc_auc_score(label_all, 1-prob_all_soft[:,0])
 
+    val_acc_org = val_acc / len(val_set)
+
+    return acc_valid,auc_valid,val_acc_org
 
 
 
-    test_acc_org = test_acc / len(test_set)
-
-    # test_auc = roc_auc_score(label_all,prob_all)
-    # test_auc = roc_auc_score(label_all,prob_all_soft)
-    # print('//////////////',label_all.shape,prob_all_soft.shape)
-
-    # val_th, val_apcer, val_bpcer, val_acer, val_auc = evalute_performances(prob_all_soft, label_all)
-
-    # return test_acc,val_auc
-    return acc_valid,auc_valid,test_acc_org
-
-
-
-#将tensor分割成块
+##### split tensor
 def window_partition(x,window_size):
     '''
     Args:
@@ -337,7 +202,7 @@ def window_partition(x,window_size):
         windows:(num_windows*B,window_size,window_size,C)
     '''
     x = x.permute(0,2,3,1)
-    # print('+++++++++++',x.shape)
+    # print('//+++++++++++',x.shape)
     B,H,W,C = x.shape
     x = x.view(B,H // window_size, window_size, W // window_size, window_size,C)
     # print('+++++++++++',x.shape)
@@ -346,12 +211,7 @@ def window_partition(x,window_size):
     return windows
 
 
-
-from evaluationmetric import AvgrageMeter, evalute_performances, evalute_threshold_based
-
-from FeatureMap2Heatmap import featuremap2heatmap
-##############define cosine distance
-from sklearn.metrics.pairwise import cosine_similarity
+#######################training
 def train(num_epochs):
     best_acc = 0.0
     best_auc = 0.0
@@ -359,13 +219,9 @@ def train(num_epochs):
         model.train()
         train_acc = 0.0
         train_loss = 0.0
-
-        print("_____________",train_dataloader)
+        print(train_dataloader)
         for i, (images, labels) in enumerate(train_dataloader):
 
-            # print('____________',os.getcwd(images))
-
-            # 若GPU可用，将图像和标签移往GPU
             i+=1
             # images = images.to(device3)
             # labels = labels.to(device3)
